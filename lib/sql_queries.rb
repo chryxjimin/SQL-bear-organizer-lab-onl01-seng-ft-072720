667bf391ed4b62b80c4c5a748eae3e0d746ad5ad
Christie Yoon <cleex1999@gmail.com>
2020-08-25 02:00:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,8 +12,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "SELECT MAX(age) FROM bears"
-  return bears.name
-  return bears.age
+  return "SELECT bear.name, bear.age"
 end
 
 def select_youngest_bear_and_returns_name_and_age
